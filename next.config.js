a88f30cf4b82f830457c2c66a2494a9cd4d75335
MAX-786 <hussainmohammad328@gmail.com>
2022-12-29 14:14:47 +0530

    add dotenv plugin in next for simplicity sake

--- next.config.js
+++ next.config.js
@@ -4,10 +4,12 @@ const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
 const webpack = require("webpack");
 const path = require("path");
+const {parsed: myenv} = require("dotenv");
 
 module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
   webpack(config, options) {
     config.resolve.modules.push(path.resolve("./"));
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
     return config;
   },
 });
